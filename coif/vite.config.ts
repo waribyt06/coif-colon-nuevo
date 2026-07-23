f7943cb44c64ae7913ce7e5904c2ee0e2b449377
waribyt06 <henryjustin712@gmail.com>
2026-07-22 21:26:16 -0500

    Fix: agregar base path para GitHub Pages

--- coif/vite.config.ts
+++ coif/vite.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/coif-colon-nuevo/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
